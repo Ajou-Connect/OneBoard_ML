5c94e1ff2aaebee5300190d3d2077bdee8d44357
minwoo <alsdndia789@ajou.ac.kr>
2021-12-12 20:48:19 +0900

    flask

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, Response, request  ## flask 라이브러리에서 Flask import
+import imutils
+import cv2
+import socket
+import pickle
+import struct
+import winsound as sd
+import threading
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def beepsound():
+    fr = 2000
+    du = 1000
+    sd.Beep(fr, du)
+
+
+def Recv(socket):
+    while True:
+        get_data = socket.recv(4)
+        length = int.from_bytes(get_data, "little")
+        get_data = socket.recv(length)
+        msg = get_data.decode()
+        print('Received from : ', msg)
+        if msg == 'alarm':
+            beepsound()
+
+def Send(socket, camera, encode_param):
+    while True:
+        ret, image_o = camera.read()
+        ret, buffer = cv2.imencode('.jpg', image_o, encode_param)
+        data = pickle.dumps(buffer, 0)
+        size = len(data)
+        socket.sendall(struct.pack(">L", size) + data)
+
+def Show():
+    while True:
+        ret, image_o = camera.read()
+        ret, buffer = cv2.imencode('.jpg', image_o, encode_param)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen_frames():
+    ip = '115.85.182.194'  # ip 주소 115.85.182.194
+    port = 8090  # port
+    camera = cv2.VideoCapture(0)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port))
+
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 200)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+    t1 = threading.Thread(target=Send, args= (client_socket, camera, encode_param))
+    t2 = threading.Thread(target=Recv, args= (client_socket,))
+    # t3 = threading.Thread(target=Show, args= ())
+    t1.daemon = True
+    t2.daemon = True
+    # t3.daemon = True
+    t2.start()
+    t1.start()
+    # t3.start()
+    # yield (b'--frame\r\n'
+    #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
+
+# In[ ]:
